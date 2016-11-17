0e980392c1aa66f0e8a14b38e94fb550df09432e
baazz786 <bharadwaz.shona@gmail.com>
2016-11-17 13:44:36 -0500

    sample jav prog to print heloworld

---
+++ src/com/egen/fullStack/Project/movieflix/sample.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.egen.fullStack.Project.movieflix;
+
+/**
+ * @author Bharadwaz
+ *
+ */
+public class sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
